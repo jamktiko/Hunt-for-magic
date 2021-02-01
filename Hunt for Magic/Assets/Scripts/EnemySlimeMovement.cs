b6f84ebb65caedbc5b6eb3ef3d454cb1295bba25
Riku Juvonen <reigos12@gmail.com>
2021-02-01 16:25:57 +0200

    Movement Scripts

---
+++ Hunt for Magic/Assets/Scripts/EnemySlimeMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySlimeMovement : MonoBehaviour
+{
+    private float jump = 10;
+    private float speed = 10;
+    private Rigidbody enemyRB;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+        player = GameObject.Find("PlayerCharacter");
+        //make slime rigid
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRB.AddForce(lookDirection * speed);
+    }
+}
