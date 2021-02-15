9b46315565c8944af73a44bbc4ee522f1c223c53
Riku Juvonen <reigos12@gmail.com>
2021-02-15 13:47:01 +0200

    LightingSpellFinish

--- Hunt for Magic/Assets/Scripts/LightingSpell.cs
+++ Hunt for Magic/Assets/Scripts/LightingSpell.cs
@@ -5,49 +5,26 @@ using UnityEngine;
 public class LightingSpell : MonoBehaviour
 {
     [SerializeField]
-    private float _damageAmount = 1f;
-    private float maxCount = 3;
-    private float ammoChanger = 1;
-    private float chargeCounter = 0;
-    private float chargeBoost = 1.3f;
-    private float currentAmmo;
-    private float actualDamage;
+    private float _damageAmount = 15f;
     private bool chargeHold = false;
+    private Transform _castingPoint;
+    private float speed = 15f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        _castingPoint = GameObject.Find("CastingPoint").GetComponent<Transform>();
+        gameObject.GetComponent<Rigidbody>().AddForce(_castingPoint.forward * speed, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Object.Destroy(gameObject, 0.5f);
-        if (Input.GetMouseButtonDown(0))
-        {
-            chargeHold = true;
-        }
-        else
-        {
-            chargeHold = false;
-            actualDamage = _damageAmount * chargeCounter * chargeBoost;
-            chargeCounter = 0;
-        }
+        spellCharger();
+        Destroy(gameObject, 0.7f);
     }
 
-    private void spellCharger()
-    {
-        
-        for (int i = 0; i <= currentAmmo; i++)
-        {
-            if (chargeHold)
-            {
-                chargeCounter = chargeCounter + 1;
-                chargeDelay(chargeCounter);
-            }
-        }
-    }
-    private void OnCollisionEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         var enemy = other.gameObject.GetComponent<Rigidbody>();
 
@@ -55,27 +32,24 @@ public class LightingSpell : MonoBehaviour
 
         if (enemy != null)
         {
-            currentAmmo = currentAmmo - ammoChanger;
-            _damageAmount = actualDamage;
             enemyHealth.AddDamage(_damageAmount);
             Destroy(gameObject);
         }
     }
 
-    IEnumerator chargeDelay(float chargeCounter)
+    private void spellCharger()
     {
-        yield return new WaitForSeconds(1.2f);
-        if (chargeCounter == currentAmmo)
-        {
-            currentAmmo = currentAmmo - chargeCounter;
-        }
-        else chargeCounter++;
+        chargeHold = true;
+            if (chargeHold)
+            {
+                Cooldown();
+                chargeHold = false;
+            }
+            else chargeHold = false;
     }
 
     IEnumerator Cooldown()
     {
         yield return new WaitForSeconds(2.4f);
-        currentAmmo = currentAmmo + ammoChanger;
-        yield return currentAmmo;
     }
 }
--- Hunt for Magic/Assets/Scripts/SpellCasting.cs
+++ Hunt for Magic/Assets/Scripts/SpellCasting.cs
@@ -19,6 +19,12 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
     [SerializeField]
     private float _throwForce = 20f;
 
+    public float ammoCount = 1;
+    public float ammoChanger = 1;
+    public float maxAmmo = 3;
+    public bool ammoChangerCooldown = false;
+    public bool canCharge = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +35,17 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetButton("Fire1"))
+        if (!ammoChangerCooldown)
+        {            
+            if (ammoCount < maxAmmo)
+            {
+                canCharge = true;
+                ammoChangerCooldown = true;
+                StartCoroutine(ammoChangerInitiate());
+            }
+        }
+
+        if (Input.GetButtonDown("Fire1"))
         {
             if (_spellPrefab.name == "WindEffect")
             {
@@ -37,7 +53,6 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
                 {
                     return;
                 }
-
                 Instantiate(_spellPrefab, _castingPoint.position, _castingPoint.rotation);
 
                 _spellCooldown = true;
@@ -66,10 +81,6 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
                 Invoke("Endcooldown", _spellInterval);
             }
 
-        }
-
-        if (Input.GetButton("Fire1"))
-        {
             if (_spellPrefab.name == "Electricity")
             {
                 if (_spellCooldown)
@@ -77,25 +88,36 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
                     return;
                 }
 
-                Instantiate(_spellPrefab, _castingPoint.position, _castingPoint.rotation);
+                if (ammoCount >= 1) 
+                {
+                    ammoCount = ammoCount - ammoChanger;
+                    Instantiate(_spellPrefab, _castingPoint.position, _castingPoint.rotation);
+                }
 
                 _spellCooldown = true;
 
                 Invoke("EndCooldown", _spellInterval);
             }
-        }
-        if (_spellPrefab.name == "WaterWavePrefab")
-        {
-            WaterSpell.SpawnSpell(_spellPrefab, _castingPoint, 2);
-
-            _spellCooldown = true;
-
-            Invoke("Endcooldown", _spellInterval);
-        }
+        } 
     }
 
     public void EndCooldown()
     {
         _spellCooldown = false;
+    }
+
+    IEnumerator ammoChangerInitiate()
+    {
+        yield return new WaitForSeconds(3.5f);
+        if (canCharge)
+        {
+            canCharge = false;
+            if (ammoChangerCooldown)
+            {            
+                ammoChangerCooldown = false;
+                if (ammoCount < maxAmmo)
+                ammoCount = ammoCount + ammoChanger;
+            }
+        }
     }
 }
