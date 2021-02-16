b967545d2e051d4eeb5cbd398f0f099eb4235d68
Nikotin94 <Nikotin94@github.com>
2021-02-16 11:59:09 +0200

    Fireball-scripti

---
+++ Hunt for Magic/Assets/Scripts/FireballSpell.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballSpell : MonoBehaviour
+{
+    [SerializeField]
+    private float _damageAmount = 5f;
+
+    private GameObject _explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _explosion = Resources.Load<GameObject>("Prefabs/Explosion");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        var enemy = other.gameObject.GetComponent<Rigidbody>();
+
+        var enemyHealth = other.gameObject.GetComponent<HealthSystem>();
+
+        if (enemy != null && enemy.name != "PlayerCharacter")
+        {
+            enemyHealth.AddDamage(_damageAmount);
+            Instantiate(_explosion, transform.position, Quaternion.identity);
+        }
+    }
+}
