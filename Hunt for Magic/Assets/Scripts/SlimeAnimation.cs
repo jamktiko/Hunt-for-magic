e642a9dd7b45e83b1b7a7101fd52bd9e7d4094e0
Nikotin94 <n3177@student.jamk.fi>
2021-02-25 16:10:47 +0200

    Fixejä

--- Hunt for Magic/Assets/Scripts/SlimeAnimation.cs
+++ Hunt for Magic/Assets/Scripts/SlimeAnimation.cs
@@ -6,24 +6,24 @@ public class SlimeAnimation : MonoBehaviour
 {
     public Animator _anim;
 
-    private GameObject _slime;
+    private Rigidbody _slime;
 
     // Start is called before the first frame update
     void Start()
     {
-        _slime = GameObject.Find("EnemySlimePrefab");
+        _slime = gameObject.GetComponentInParent<Rigidbody>();
         _anim = _slime.GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        if (_slime.GetComponent<Rigidbody>().velocity != Vector3.zero)
+        if (_slime.velocity != Vector3.zero)
         {
             _anim.SetBool("Jump", true);
         }
 
-        if (_slime.GetComponent<Rigidbody>().velocity == Vector3.zero)
+        if (_slime.velocity == Vector3.zero)
         {
             _anim.SetBool("Jump", false);
         }
