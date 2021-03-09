84afb56254a2f35fef34762777e6d113a4e1745f
Nikotin94 <n3177@student.jamk.fi>
2021-03-09 15:07:45 +0200

    Enemy HP Barit kuntoon

---
+++ Hunt for Magic/Assets/Scripts/EnemyHealthbar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthbar : MonoBehaviour
+{
+    [SerializeField]
+    private Image _healthBarImage;
+
+    [SerializeField]
+    private GameObject _enemyHealth;
+
+    private HealthSystem _enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemy = _enemyHealth.GetComponentInParent<HealthSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = Camera.main.WorldToScreenPoint(_enemyHealth.transform.position);
+        transform.position = position;
+
+        if (position.z > 0 && position.z < 20)
+        {
+            gameObject.GetComponent<Image>().enabled = true;
+            _healthBarImage.enabled = true;
+        }
+
+        else
+        {
+            gameObject.GetComponent<Image>().enabled = false;
+            _healthBarImage.enabled = false;
+
+        }
+
+        _healthBarImage.fillAmount = _enemy.health / 100;
+
+        if (_enemy.health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
