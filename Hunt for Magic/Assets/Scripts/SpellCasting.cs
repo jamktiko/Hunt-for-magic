1fec61fbce26f5f4b1859bc679bdb375bed3fce6
Riku Juvonen <reigos12@gmail.com>
2021-03-09 10:56:58 +0200

    Maanantai updates

--- Hunt for Magic/Assets/Scripts/SpellCasting.cs
+++ Hunt for Magic/Assets/Scripts/SpellCasting.cs
@@ -25,10 +25,12 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
     public float ammoChanger = 1;
     public float maxAmmo = 3;
     public float chargeCounter = 0;
+    public float spellCharge = 0;
     public bool ammoChangerCooldown = false;
     public bool canCharge = false;
-    private bool canChargeSpell = false;
-    private bool chargeChancerCooldown = false;
+    public bool canChargeSpell = false;
+    public bool chargeChancerCooldown = false;
+    private bool alreadyCast = false;
 
     // Start is called before the first frame update
     void Start()
@@ -98,6 +100,7 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
 
             if (_spellPrefab.name == "WindEffect")
             {
+                _spellInterval = 1f;
                 if (_spellCooldown)
                 {
                     return;
@@ -112,6 +115,7 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
             if (_spellPrefab.name == "Flamethrower_particle")
             {
                 _player.GetComponent<EnergySystem>().ReduceEnergy(1f);
+                _spellInterval = 1.5f;
 
                 if (_player.GetComponent<EnergySystem>()._currentEnergy < 5)
                 {
@@ -123,6 +127,7 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
 
             if (_spellPrefab.name == "Waterwave" && PlayerCharacterController.isGrounded)
             {
+                _spellInterval = 2f;
                 if (_spellCooldown)
                 {
                     return;
@@ -136,6 +141,7 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
 
             if (_spellPrefab.name == "Electricity")
             {
+                _spellInterval = 0.8f;
                 if (_spellCooldown)
                 {
                     return;
@@ -154,6 +160,7 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
 
             if (_spellPrefab.name == "Fireball")
             {
+                _spellInterval = 3f;
                 if (_spellCooldown)
                 {
                     return;
@@ -168,6 +175,7 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
 
             if (_spellPrefab.name == "ChainLightning")
             {
+                _spellInterval = 2f;
                 if (ammoCount > 0)
                 {
                     if (_spellCooldown)
@@ -185,6 +193,7 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
 
             if (_spellPrefab.name == "LightningBolt")
             {
+                _spellInterval = 1.7f;
                 if (ammoCount > 0)
                 {
                     if (_spellCooldown)
@@ -202,21 +211,35 @@ public class SpellCasting : MonoBehaviour  // Tämä scripti liitetään pelaaja
         }
         if (Input.GetButtonUp("Fire1") || chargeCounter == 6)
         {
-            if (_spellPrefab.name == "ChainLightning")
+            if (!alreadyCast && chargeCounter != 0)
             {
-                canChargeSpell = false;
-                Instantiate(_spellPrefab, _castingPoint.position, _castingPoint.rotation);
-                chargeCounter = 0;
-                Invoke("EndCooldown", _spellInterval);
+                if (_spellPrefab.name == "ChainLightning")
+                {
+                    spellCharge = chargeCounter;
+                    canChargeSpell = false;
+                    Instantiate(_spellPrefab, _castingPoint.position, _castingPoint.rotation);                 
+                    alreadyCast = true;
+                    chargeChancerCooldown = false;
+                    Invoke("EndCooldown", _spellInterval);
+                    chargeCounter = 0;
+                }
+
+                if (_spellPrefab.name == "LightningBolt")
+                {
+                    spellCharge = chargeCounter;
+                    canChargeSpell = false;
+                    Instantiate(_spellPrefab, _castingPoint.position, _castingPoint.rotation);
+                    alreadyCast = true;
+                    chargeChancerCooldown = false;
+                    Invoke("EndCooldown", _spellInterval);
+                    chargeCounter = 0;
+                }
+            }
+            else if (alreadyCast)
+            {
+                alreadyCast = false;
             }
 
-            if (_spellPrefab.name == "LightningBolt")
-            {
-                canChargeSpell = false;
-                Instantiate(_spellPrefab, _castingPoint.position, _castingPoint.rotation);
-                chargeCounter = 0;
-                Invoke("EndCooldown", _spellInterval);
-            }
         }
     }
 
