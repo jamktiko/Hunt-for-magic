59c2b2d6726aa1d5044dbc26fb8aca6a26093e7a
rudezki <46998792+rudezki@users.noreply.github.com>
2021-03-10 11:25:25 +0200

    Cooldown lisätty
    myös waterspelliin muutoksia

---
+++ Hunt for Magic/Assets/Scripts/Cooldown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cooldown : MonoBehaviour
+{
+    private static Image cooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cooldown = GetComponent<Image>();
+        cooldown.fillAmount = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SpellCasting._spellCooldown == true)
+        {
+            cooldown.enabled = true;
+            cooldown.fillAmount -= 1.0f * Time.deltaTime;
+        }
+        else
+        {
+            cooldown.enabled = false;
+            cooldown.fillAmount = 1.0f;
+        }
+
+    }
+}
