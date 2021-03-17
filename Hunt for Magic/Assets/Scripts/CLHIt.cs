89fc9c69bfdf71a44339df0bad2a9cdeafe27f6a
Riku Juvonen <reigos12@gmail.com>
2021-03-17 09:59:23 +0200

    Chainlighting update

---
+++ Hunt for Magic/Assets/Scripts/CLHIt.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CLHIt : MonoBehaviour
+{
+    public bool firstHit;
+    public bool clHit;
+    private Vector3 scaleChange, positionChange;
+    public Object Target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstHit = true;
+        clHit = false;
+        gameObject.GetComponent<SphereCollider>();
+        scaleChange = new Vector3(0.08f, 0.08f, 0.08f);
+        positionChange = new Vector3(0, -0.08f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.localScale += scaleChange;
+        gameObject.transform.position += positionChange;
+        Destroy(gameObject, 1.5f);
+    }
+
+    //Find target
+    void OnTriggerEnter(Collider other)
+    {
+        var enemy = other.gameObject.GetComponent<EnemySlimeMovement>();
+        var target = other.gameObject.GetComponent<Rigidbody>();
+
+        clHit = enemy;
+
+        if (clHit)
+        {
+            
+        }
+        else if (!clHit)
+        {
+            Target = target;
+        }
+    }
+}
--- Hunt for Magic/Assets/Scripts/ChainLightingSpell.cs
+++ Hunt for Magic/Assets/Scripts/ChainLightingSpell.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class ChainLightingSpell : MonoBehaviour
 {
     [SerializeField]
-    private float _damageAmount = 25f;
+    private float _damageAmount = 30f;
     private Transform _castingPoint;
     private float speed = 20f;
     public float chargeCounter;
-    private bool targetFound = false;
-    private GameObject target;
-    private GameObject[] TargetList;
+    private Object targetFound;
+    private GameObject enemy;
+    private Rigidbody enemyRB;
+    public bool firstHit;
+    private Object enemyFinder;
+    private Object Target;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyFinder = Resources.Load("Prefabs/EnemyFinder");
+        firstHit = false;
         GameObject player = GameObject.Find("PlayerCharacter");        
         chargeCounter = player.GetComponent<SpellCasting>().chargeCounter;
         _castingPoint = GameObject.Find("CastingPoint").GetComponent<Transform>();
@@ -26,13 +30,27 @@ public class ChainLightingSpell : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {       
+        enemy = GameObject.Find("EnemyFinder");
+        targetFound = enemy.gameObject.GetComponent<CLHIt>();
+        Target = targetFound;
+
+        if (firstHit)
+        {
+            Vector3 Target = (enemyRB.transform.position - transform.position).normalized;
+            gameObject.GetComponent<Rigidbody>().AddForce(Target * speed, ForceMode.Impulse);
+        }
+
         Destroy(gameObject, 2.1f);
     }
 
     private void OnTriggerEnter(Collider other)
 
     {
+        if (!firstHit)
+        {
+            firstHit = true;
+        }
         var enemy = other.gameObject.GetComponent<Rigidbody>();
 
         var enemyHealth = other.gameObject.GetComponent<HealthSystem>();
@@ -43,6 +61,8 @@ public class ChainLightingSpell : MonoBehaviour
             {
                 enemyHealth.AddDamage(_damageAmount);
                 chargeCounter--;
+                transform.position = Vector3.zero;
+                Instantiate(enemyFinder, transform.position, transform.rotation);
             }
         }
     }
@@ -57,7 +77,7 @@ public class ChainLightingSpell : MonoBehaviour
         {
             enemyHealth.AddDamage(_damageAmount);
 
-            if(chargeCounter == 1)
+            if(chargeCounter >= 1)
             {
                 Destroy(gameObject);
             }
