5d5448c3b96ec29844ca5a90d92ce6429f4000b4
Nikotin94 <n3177@student.jamk.fi>
2021-03-31 12:35:01 +0300

    Sumunäkyvyys slimellä huonommaksi + tulipallolle AoE-damage

---
+++ Hunt for Magic/Assets/Scripts/FogVisibility.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogVisibility : MonoBehaviour
+{
+    [SerializeField]
+    private float _slimeRange = 5f;
+
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.name.Contains("EnemySlimePrefab"))
+        {
+            other.GetComponent<EnemySlimeMovement>()._range = _slimeRange;
+            yield return new WaitForSeconds(6.9f);
+            other.GetComponent<EnemySlimeMovement>()._range = other.GetComponent<EnemySlimeMovement>()._maxRange;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name.Contains("EnemySlimePrefab"))
+        {
+            other.GetComponent<EnemySlimeMovement>()._range = other.GetComponent<EnemySlimeMovement>()._maxRange;
+        }
+    }
+}
