31671df373b423400c16b047c2a5919203596927
Nikotin94 <n3177@student.jamk.fi>
2021-04-12 14:40:23 +0300

    Icewall, aloitushuoneen spellit, lightningboltin cooldown ja fixejä

--- Hunt for Magic/Assets/Scripts/IcewallSpell.cs
+++ Hunt for Magic/Assets/Scripts/IcewallSpell.cs
@@ -16,12 +16,15 @@ public class IcewallSpell : MonoBehaviour
 
     private Object _iceWall;
 
+    private Object _iceTrigger;
+
     // Start is called before the first frame update
     void Start()
     {
         _castingPoint = GameObject.Find("CastingPoint").GetComponent<Transform>();
         _waterCastingPoint = GameObject.Find("WaterCastingPoint").GetComponent<Transform>();
         gameObject.GetComponent<Rigidbody>().AddForce(_castingPoint.forward * _speed, ForceMode.Impulse);
+        _iceTrigger = Resources.Load("Prefabs/Icewall_Trigger");
         _iceWall = Resources.Load("Prefabs/IcyWall");
     }
 
@@ -33,22 +36,12 @@ public class IcewallSpell : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        var enemy = other.gameObject.GetComponent<Rigidbody>();
-
-        var enemyHealth = other.gameObject.GetComponent<HealthSystem>();
-
-        if (enemy != null && enemy.tag == "Monster")
+        if (other.tag == "Ground" || other.tag == "Wood")
         {
-            enemyHealth.AddDamage(_damageAmount);
+            Object damageTrigger = Instantiate(_iceTrigger, transform.position, Quaternion.identity);
+            Object icewall = Instantiate(_iceWall, transform.TransformPoint(0, -1, 0), _waterCastingPoint.rotation);
 
-            Object icewall = Instantiate(_iceWall, transform.position, _waterCastingPoint.rotation);
-            Destroy(icewall, 8f);
-
-            Destroy(gameObject);
-        }
-        else if (other.tag == "Ground" || other.tag == "Wood")
-        {
-            Object icewall = Instantiate(_iceWall, transform.position, _waterCastingPoint.rotation);
+            Destroy(damageTrigger, 1f);
             Destroy(icewall, 8f);
 
             Destroy(gameObject);
