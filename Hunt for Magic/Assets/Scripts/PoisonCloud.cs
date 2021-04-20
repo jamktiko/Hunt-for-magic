93b7a5fce3f91dfba112332572e46669c15742d9
Nikotin94 <n3177@student.jamk.fi>
2021-04-20 16:43:06 +0300

    Plantien alkua

---
+++ Hunt for Magic/Assets/Scripts/PoisonCloud.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonCloud : MonoBehaviour
+{
+    private bool _cooldown;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!_cooldown)
+            {
+                other.GetComponent<HealthSystem>().AddDamage(2f);
+                _cooldown = true;
+                Invoke("CooldownEnd", 0.4f);
+            }
+        }
+    }
+
+    void CooldownEnd()
+    {
+        _cooldown = false;
+    }
+}
