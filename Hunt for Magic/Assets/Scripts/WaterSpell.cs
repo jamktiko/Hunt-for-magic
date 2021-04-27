8ccdd29ae4eb048bbb134bb6adab407eaad37d7e
rudezki <46998792+rudezki@users.noreply.github.com>
2021-04-27 13:00:13 +0300

    Update WaterSpell.cs

--- Hunt for Magic/Assets/Scripts/WaterSpell.cs
+++ Hunt for Magic/Assets/Scripts/WaterSpell.cs
@@ -27,7 +27,7 @@ public class WaterSpell : MonoBehaviour
         _waterCastingPoint = GameObject.Find("WaterCastingPoint").GetComponent<Transform>();
         gameObject.GetComponent<Rigidbody>().AddForce(_waterCastingPoint.forward * _speed, ForceMode.Impulse);
         gameObject.GetComponent<SphereCollider>();
-        scaleChange = new Vector3(-0.01f, 0.02f, 0);
+        scaleChange = new Vector3(-0.01f, 0.002f, 0);
         positionChange = new Vector3(0, -0.0001f, 0);
         StartCoroutine(DamageFizzle());
         _waterPool = Resources.Load("Prefabs/GroundWater");
@@ -37,7 +37,7 @@ public class WaterSpell : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Object.Destroy(gameObject, 6.0f);
+        Destroy(gameObject, 5.0f);
         if (Time.timeScale == 1)
         {
             if (_waterSpellCollider != null)
