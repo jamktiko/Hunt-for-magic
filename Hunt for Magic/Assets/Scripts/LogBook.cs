bc58ab0c5262cb05345f42c098e7eb43884111ba
rudezki <46998792+rudezki@users.noreply.github.com>
2021-04-28 13:00:34 +0300

    Logbook work

--- Hunt for Magic/Assets/Scripts/LogBook.cs
+++ Hunt for Magic/Assets/Scripts/LogBook.cs
@@ -12,22 +12,37 @@ public class LogBook : MonoBehaviour
     [SerializeField]
     private Button _enemiesButton;
     [SerializeField]
-    private Button _backButton;
+    private Button _arrowLeftButton;
     [SerializeField]
-    private Button _forwardButton;
+    private Button _arrowRightButton;
+    [SerializeField]
+    private Button _backButton;
 
+    [SerializeField]
+    private Image _itemGrid;
+    public GameObject _mainMenu;
     public AudioSource _menuSrc;
     public AudioClip _menuClick;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _backButton.onClick.AddListener(Back);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void Back()
+    {
+        _menuSrc.PlayOneShot(_menuClick);
+        gameObject.SetActive(false);
+        _mainMenu.SetActive(true);
+    }
+    private void Items()
+    {
+
     }
 }
