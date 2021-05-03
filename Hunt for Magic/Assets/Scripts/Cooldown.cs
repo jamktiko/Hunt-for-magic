e0412e637aaa6395c5a4518afb67e9a614a895dc
Nikotin94 <n3177@student.jamk.fi>
2021-05-03 12:11:48 +0300

    Kokonaan uusittu cooldown-koodi

--- Hunt for Magic/Assets/Scripts/Cooldown.cs
+++ Hunt for Magic/Assets/Scripts/Cooldown.cs
@@ -10,122 +10,382 @@ public class Cooldown : MonoBehaviour
     public Image cooldown2;
     private GameObject _player;
     public GameObject _weaponArea;
-    private Image cooldown;
-    public int _slot;
+
+    private Object _spell0;
+    private Object _spell1;
+    private Object _spell2;
+
+    private float _electricity = 0.8f;
+    private float _wind = 1f;
+    private float _lightningbolt = 1.7f;
+    private float _water = 2f;
+    private float _oil = 2f;
+    private float _chainLightning = 2f;
+    private float _fireball = 3f;
+    private float _iceWall = 8f;
 
     // Start is called before the first frame update
     void Start()
     {
         _player  = GameObject.Find("PlayerCharacter");
+
+        cooldown0.enabled = false;
+        cooldown0.fillAmount = 1f;
+
+        cooldown1.enabled = false;
+        cooldown1.fillAmount = 1f;
+
+        cooldown2.enabled = false;
+        cooldown2.fillAmount = 1f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (_weaponArea.GetComponent<SpellBehaviour>()._spell0 != null)
+        {
+            _spell0 = _weaponArea.GetComponent<SpellBehaviour>()._spell0;
+        }
+        if (_weaponArea.GetComponent<SpellBehaviour>()._spell1 != null)
+        {
+            _spell1 = _weaponArea.GetComponent<SpellBehaviour>()._spell1;
+        }
+        if (_weaponArea.GetComponent<SpellBehaviour>()._spell2 != null)
+        {
+            _spell2 = _weaponArea.GetComponent<SpellBehaviour>()._spell2;
+        }
+
         if (_player.GetComponent<SpellCasting>()._windCooldown)
         {
-            if (_player.GetComponent<SpellCasting>()._spellPrefab.name == "WindEffect")
+            if (_spell0 != null && _spell0.name == "WindEffect")
             {
-                _slot = SpellBehaviour._activeSlot;
+                cooldown0.enabled = true;
+                cooldown0.fillAmount -= 1 / _wind * Time.deltaTime;
             }
-
-            Slot();
+            else if (_spell1 != null && _spell1.name == "WindEffect")
+            {
+                cooldown1.enabled = true;
+                cooldown1.fillAmount -= 1 / _wind * Time.deltaTime;
+            }
+            else if (_spell2 != null && _spell2.name == "WindEffect")
+            {
+                cooldown2.enabled = true;
+                cooldown2.fillAmount -= 1 / _wind * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (_spell0 != null && _spell0.name == "WindEffect")
+            {
+                cooldown0.enabled = false;
+                cooldown0.fillAmount = 1f;
+            }
+            else if (_spell1 != null && _spell1.name == "WindEffect")
+            {
+                cooldown1.enabled = false;
+                cooldown1.fillAmount = 1f;
+            }
+            else if (_spell2 != null && _spell2.name == "WindEffect")
+            {
+                cooldown2.enabled = false;
+                cooldown2.fillAmount = 1f;
+            }
         }
 
         if (_player.GetComponent<SpellCasting>()._waterCooldown)
         {
-            if (_player.GetComponent<SpellCasting>()._spellPrefab.name == "Waterwave")
+            if (_spell0 != null && _spell0.name == "Waterwave")
             {
-                _slot = SpellBehaviour._activeSlot;
+                cooldown0.enabled = true;
+                cooldown0.fillAmount -= 1 / _water * Time.deltaTime;
             }
-
-            Slot();
+            else if (_spell1 != null && _spell1.name == "Waterwave")
+            {
+                cooldown1.enabled = true;
+                cooldown1.fillAmount -= 1 / _water * Time.deltaTime;
+            }
+            else if (_spell2 != null && _spell2.name == "Waterwave")
+            {
+                cooldown2.enabled = true;
+                cooldown2.fillAmount -= 1 / _water * Time.deltaTime;
+            }
         }
-
-        if (_player.GetComponent<SpellCasting>()._elecCooldown)
+        else
         {
-            if (_player.GetComponent<SpellCasting>()._spellPrefab.name == "Electricity")
+            if (_spell0 != null && _spell0.name == "Waterwave")
             {
-                _slot = SpellBehaviour._activeSlot;
+                cooldown0.enabled = false;
+                cooldown0.fillAmount = 1f;
             }
-
-            Slot();
-        }
-
-        if (_player.GetComponent<SpellCasting>()._fireballCooldown)
-        {
-            if (_player.GetComponent<SpellCasting>()._spellPrefab.name == "Fireball")
+            else if (_spell1 != null && _spell1.name == "Waterwave")
             {
-                _slot = SpellBehaviour._activeSlot;
+                cooldown1.enabled = false;
+                cooldown1.fillAmount = 1f;
             }
-
-            Slot();
-        }
-
-        if (_player.GetComponent<SpellCasting>()._lightningboltCooldown)
-        {
-            if (_player.GetComponent<SpellCasting>()._spellPrefab.name == "LightningBolt")
+            else if (_spell2 != null && _spell2.name == "Waterwave")
             {
-                _slot = SpellBehaviour._activeSlot;
+                cooldown2.enabled = false;
+                cooldown2.fillAmount = 1f;
             }
-
-            Slot();
-        }
-
-        if (_player.GetComponent<SpellCasting>()._chainlightningCooldown)
-        {
-            if (_player.GetComponent<SpellCasting>()._spellPrefab.name == "ChainLightning")
-            {
-                _slot = SpellBehaviour._activeSlot;
-            }
-
-            Slot();
-        }
-
-        if (_player.GetComponent<SpellCasting>()._icewallCooldown)
-        {
-            if (_player.GetComponent<SpellCasting>()._spellPrefab.name == "IceWallSpell")
-            {
-                _slot = SpellBehaviour._activeSlot;
-            }
-
-            Slot();
         }
 
         if (_player.GetComponent<SpellCasting>()._oilCooldown)
         {
-            if (_player.GetComponent<SpellCasting>()._spellPrefab.name == "OilSpell")
+            if (_spell0 != null && _spell0.name == "OilSpell")
             {
-                _slot = SpellBehaviour._activeSlot;
+                cooldown0.enabled = true;
+                cooldown0.fillAmount -= 1 / _oil * Time.deltaTime;
             }
-
-            Slot();
-        }
-
-        if (_player.GetComponent<EnergySystem>()._currentEnergy < 5f)
-        {
-            if (_player.GetComponent<SpellCasting>()._spellPrefab.name == "Flamethrower_particle")
+            else if (_spell1 != null && _spell1.name == "OilSpell")
             {
-                _slot = SpellBehaviour._activeSlot;
+                cooldown1.enabled = true;
+                cooldown1.fillAmount -= 1 / _oil * Time.deltaTime;
             }
+            else if (_spell2 != null && _spell2.name == "OilSpell")
+            {
+                cooldown2.enabled = true;
+                cooldown2.fillAmount -= 1 / _oil * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (_spell0 != null && _spell0.name == "OilSpell")
+            {
+                cooldown0.enabled = false;
+                cooldown0.fillAmount = 1f;
+            }
+            else if (_spell1 != null && _spell1.name == "OilSpell")
+            {
+                cooldown1.enabled = false;
+                cooldown1.fillAmount = 1f;
+            }
+            else if (_spell2 != null && _spell2.name == "OilSpell")
+            {
+                cooldown2.enabled = false;
+                cooldown2.fillAmount = 1f;
+            }
+        }
 
-            Slot();
+        if (_player.GetComponent<SpellCasting>()._elecCooldown)
+        {
+            if (_spell0 != null && _spell0.name == "Electricity")
+            {
+                cooldown0.enabled = true;
+                cooldown0.fillAmount -= 1 / _electricity * Time.deltaTime;
+            }
+            else if (_spell1 != null && _spell1.name == "Electricity")
+            {
+                cooldown1.enabled = true;
+                cooldown1.fillAmount -= 1 / _electricity * Time.deltaTime;
+            }
+            else if (_spell2 != null && _spell2.name == "Electricity")
+            {
+                cooldown2.enabled = true;
+                cooldown2.fillAmount -= 1 / _electricity * Time.deltaTime;
+            }
         }
-    }
+        else
+        {
+            if (_spell0 != null && _spell0.name == "Electricity")
+            {
+                cooldown0.enabled = false;
+                cooldown0.fillAmount = 1f;
+            }
+            else if (_spell1 != null && _spell1.name == "Electricity")
+            {
+                cooldown1.enabled = false;
+                cooldown1.fillAmount = 1f;
+            }
+            else if (_spell2 != null && _spell2.name == "Electricity")
+            {
+                cooldown2.enabled = false;
+                cooldown2.fillAmount = 1f;
+            }
+        }
 
-    private void Slot()
-    {
-        if (_slot == 0)
+        if (_player.GetComponent<SpellCasting>()._fireballCooldown)
         {
-            cooldown0.enabled = true;
+            if (_spell0 != null && _spell0.name == "Fireball")
+            {
+                cooldown0.enabled = true;
+                cooldown0.fillAmount -= 1 / _fireball * Time.deltaTime;
+            }
+            else if (_spell1 != null && _spell1.name == "Fireball")
+            {
+                cooldown1.enabled = true;
+                cooldown1.fillAmount -= 1 / _fireball * Time.deltaTime;
+            }
+            else if (_spell2 != null && _spell2.name == "Fireball")
+            {
+                cooldown2.enabled = true;
+                cooldown2.fillAmount -= 1 / _fireball * Time.deltaTime;
+            }
         }
-        else if (_slot == 1)
+        else
         {
-            cooldown1.enabled = true;
+            if (_spell0 != null && _spell0.name == "Fireball")
+            {
+                cooldown0.enabled = false;
+                cooldown0.fillAmount = 1f;
+            }
+            else if (_spell1 != null && _spell1.name == "Fireball")
+            {
+                cooldown1.enabled = false;
+                cooldown1.fillAmount = 1f;
+            }
+            else if (_spell2 != null && _spell2.name == "Fireball")
+            {
+                cooldown2.enabled = false;
+                cooldown2.fillAmount = 1f;
+            }
         }
-        else if (_slot == 2)
+
+        if (_player.GetComponent<SpellCasting>()._lightningboltCooldown)
         {
-            cooldown2.enabled = true;
+            if (_spell0 != null && _spell0.name == "LightningBolt")
+            {
+                cooldown0.enabled = true;
+                cooldown0.fillAmount -= 1 / _lightningbolt * Time.deltaTime;
+            }
+            else if (_spell1 != null && _spell1.name == "LightningBolt")
+            {
+                cooldown1.enabled = true;
+                cooldown1.fillAmount -= 1 / _lightningbolt * Time.deltaTime;
+            }
+            else if (_spell2 != null && _spell2.name == "LightningBolt")
+            {
+                cooldown2.enabled = true;
+                cooldown2.fillAmount -= 1 / _lightningbolt * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (_spell0 != null && _spell0.name == "LightningBolt")
+            {
+                cooldown0.enabled = false;
+                cooldown0.fillAmount = 1f;
+            }
+            else if (_spell1 != null && _spell1.name == "LightningBolt")
+            {
+                cooldown1.enabled = false;
+                cooldown1.fillAmount = 1f;
+            }
+            else if (_spell2 != null && _spell2.name == "LightningBolt")
+            {
+                cooldown2.enabled = false;
+                cooldown2.fillAmount = 1f;
+            }
+        }
+
+        if (_player.GetComponent<SpellCasting>()._icewallCooldown)
+        {
+            if (_spell0 != null && _spell0.name == "IceWallSpell")
+            {
+                cooldown0.enabled = true;
+                cooldown0.fillAmount -= 1 / _iceWall * Time.deltaTime;
+            }
+            else if (_spell1 != null && _spell1.name == "IceWallSpell")
+            {
+                cooldown1.enabled = true;
+                cooldown1.fillAmount -= 1 / _iceWall * Time.deltaTime;
+            }
+            else if (_spell2 != null && _spell2.name == "IceWallSpell")
+            {
+                cooldown2.enabled = true;
+                cooldown2.fillAmount -= 1 / _iceWall * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (_spell0 != null && _spell0.name == "IceWallSpell")
+            {
+                cooldown0.enabled = false;
+                cooldown0.fillAmount = 1f;
+            }
+            else if (_spell1 != null && _spell1.name == "IceWallSpell")
+            {
+                cooldown1.enabled = false;
+                cooldown1.fillAmount = 1f;
+            }
+            else if (_spell2 != null && _spell2.name == "IceWallSpell")
+            {
+                cooldown2.enabled = false;
+                cooldown2.fillAmount = 1f;
+            }
+        }
+
+        if (_player.GetComponent<EnergySystem>()._currentEnergy < 100f)
+        {
+            if (_spell0 != null && _spell0.name == "Flamethrower_particle")
+            {
+                cooldown0.enabled = true;
+                cooldown0.fillAmount = 0 + _player.GetComponent<EnergySystem>()._currentEnergy / _player.GetComponent<EnergySystem>()._maxEnergy;
+            }
+            else if (_spell1 != null && _spell1.name == "Flamethrower_particle")
+            {
+                cooldown1.enabled = true;
+                cooldown1.fillAmount = 0 + _player.GetComponent<EnergySystem>()._currentEnergy / _player.GetComponent<EnergySystem>()._maxEnergy;
+            }
+            else if (_spell2 != null && _spell2.name == "Flamethrower_particle")
+            {
+                cooldown2.enabled = true;
+                cooldown2.fillAmount = 0 + _player.GetComponent<EnergySystem>()._currentEnergy / _player.GetComponent<EnergySystem>()._maxEnergy;
+            }
+        }
+        else
+        {
+            if (_spell0 != null && _spell0.name == "Flamethrower_particle")
+            {
+                cooldown0.enabled = false;
+                cooldown0.fillAmount = 1f;
+            }
+            else if (_spell1 != null && _spell1.name == "Flamethrower_particle")
+            {
+                cooldown1.enabled = false;
+                cooldown1.fillAmount = 1f;
+            }
+            else if (_spell2 != null && _spell2.name == "Flamethrower_particle")
+            {
+                cooldown2.enabled = false;
+                cooldown2.fillAmount = 1f;
+            }
+        }
+
+        if (_player.GetComponent<SpellCasting>()._chainlightningCooldown)
+        {
+            if (_spell0 != null && _spell0.name == "ChainLightning")
+            {
+                cooldown0.enabled = true;
+                cooldown0.fillAmount -= 1 / _chainLightning * Time.deltaTime;
+            }
+            else if (_spell1 != null && _spell1.name == "ChainLightning")
+            {
+                cooldown1.enabled = true;
+                cooldown1.fillAmount -= 1 / _chainLightning * Time.deltaTime;
+            }
+            else if (_spell2 != null && _spell2.name == "ChainLightning")
+            {
+                cooldown2.enabled = true;
+                cooldown2.fillAmount -= 1 / _chainLightning * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (_spell0 != null && _spell0.name == "ChainLightning")
+            {
+                cooldown0.enabled = false;
+                cooldown0.fillAmount = 1f;
+            }
+            else if (_spell1 != null && _spell1.name == "ChainLightning")
+            {
+                cooldown1.enabled = false;
+                cooldown1.fillAmount = 1f;
+            }
+            else if (_spell2 != null && _spell2.name == "ChainLightning")
+            {
+                cooldown2.enabled = false;
+                cooldown2.fillAmount = 1f;
+            }
         }
     }
 }
--- Hunt for Magic/Assets/Scripts/DisplayCharge.cs
+++ Hunt for Magic/Assets/Scripts/DisplayCharge.cs
@@ -52,7 +52,7 @@ public class DisplayCharge : MonoBehaviour
         if (_player.GetComponent<EnergySystem>()._currentEnergy < 100 && _player.GetComponent<SpellCasting>()._spellPrefab.name == "Flamethrower_particle")
         {
             _chargeText.enabled = true;
-            _chargeText.color = Color.Lerp(Color.gray, Color.black, _player.GetComponent<EnergySystem>()._currentEnergy / 100);
+            _chargeText.color = Color.white;
             _chargeText.text = Mathf.Round(_player.GetComponent<EnergySystem>()._currentEnergy).ToString();
         }
         else if (_player.GetComponent<SpellCasting>()._spellPrefab.name == "Electricity")
